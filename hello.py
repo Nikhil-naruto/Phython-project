bdfcf3a76b64525c917288af0682bf99453faabd
Nikhil <nikhildakamari4728@gmail.com>
2021-07-13 23:26:01 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+print("Hello Nikhil!")
+
+print('What\'s your name?')
+
+print(float(16/(float(3))))
+
+print(5*3.5)
+
+print(6+3.0)
+
+print(6+15)
+
+print(5/2)
+
+print(format('Hello', '<30'))
+
+print(format('Hello', '^30'))
+
+print(format('Hello', '>30'))
+
+luck=4
+print("lucky number = "+str(luck))
