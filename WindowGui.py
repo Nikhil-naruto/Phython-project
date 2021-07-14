e3e5df4c44f19ff0474ad197a556356af037834d
Nikhil <nikhildakamari4728@gmail.com>
2021-07-14 23:09:46 +0530

    third commit

--- WindowGui.py
+++ WindowGui.py
@@ -1,5 +1,9 @@
 from tkinter import *
 window = Tk()
+#widgets = GUI elements : buttons , lables, images
+#window = serves as a container to hold or contain these widgets
 window.title("Hello world")
 window.config(background="black")
+icon = PhotoImage(file='naruto.jpg')
+window.iconphoto(True, icon)
 window.mainloop()
