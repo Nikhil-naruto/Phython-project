a53c3d3c5bf992d5d8c624837e7414b56d2d46a9
Nikhil <nikhildakamari4728@gmail.com>
2021-07-14 19:59:05 +0530

    thrid commit

---
+++ basic.py
@@ -0,0 +1,3 @@
+name=input("What's your name?")
+age=input("Enter your age")
+print("Hello " +name+ " you are " +age+ " old.")
--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
 print("Hello Nikhil!")
 
-print('What\'s your name?')
-
 print(float(16/(float(3))))
 
 print(5*3.5)
@@ -12,6 +10,7 @@ print(6+15)
 
 print(5/2)
 
+#string formate
 print(format('Hello', '<30'))
 
 print(format('Hello', '^30'))
@@ -22,3 +21,42 @@ luck=4
 print("lucky number = "+str(luck))
 
 print(2.987/5.09875)
+
+# e is a format specifier
+print(format(3**50,'.5e'))
+
+# .2f is used show 2 decimal value only
+print(format(float(5/float(3))), '.2f')
+
+#escape sequence
+print('What\'s your name?')
+
+print("What\'s is your name ,\"My name is Nikhil.\"")
+
+print("What\'s is your Father\'s name.\n \"My Father\'s name is Suresh.\"")
+
+print("Hello guy\'s.\t How are you")
+
+print("Hello guy\'s. \
+Wlecome to python programming language.")
+
+# to assign value to variable
+x=15
+y=14
+print(x+y)
+
+#boolean
+print(20==30)
+
+print(20<30)
+
+print(87>=87.0)
+
+print('Nikhil'=='nikhil')
+
+
+
+
+
+
+
